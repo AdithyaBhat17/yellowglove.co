8908bce5efe600b32a00c758becae8787f9e84ab
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-06-02 16:34:16 +0530

    :sparkles: added lazy loading

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-import Terms from './components/Terms'
-import Privacy from './components/Privacy'
 
 import { HashRouter as Router, Route } from 'react-router-dom'
 
 import './styles/index.css'
+import { FulfillingSquareSpinner } from 'react-epic-spinners'
+
+const Terms = lazy(() => import('./components/Terms'))
+const Privacy = lazy(() => import('./components/Privacy'))
+
+const loading = (
+    <div style={{background: 'white', height: '100vh', textAlign: 'center'}}>loading...</div>
+)
 
 ReactDOM.render(
-    <Router>
-        <Route exact path="/" component={App} />
-        <Route exact path="/terms" component={Terms} />
-        <Route exact path="/privacy" component={Privacy} />
-    </Router>,
+    <Suspense fallback={loading}>
+        <Router>
+            <Route exact path="/" component={App} />
+            <Route exact path="/terms" component={() => <Terms />} />
+            <Route exact path="/privacy" component={() => <Privacy />} />
+        </Router>
+    </Suspense>,
     document.getElementById('root')
 )
